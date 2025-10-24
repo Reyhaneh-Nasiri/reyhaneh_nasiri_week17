7777784536c811c19410a35195bbc7ccb85fc08b
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-10-24 18:16:48 +0330

    feat(local-storage): add contacts in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ContactListPage from "./pages/ContactListPage/ContactListPage";
 import AddContactPage from "./pages/AddContactPage/AddContactPage";
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState("contact-list");
-  const [contacts, setContacts] = useState([
-    {
-      id: 1,
-      name: "contact1",
-      email: "contact1@gmail.com",
-      phone: "1111",
-      job: "",
-    },
-    {
-      id: 2,
-      name: "contact2",
-      email: "contact2@gmail.com",
-      phone: "2222",
-      job: "",
-    },
-    {
-      id: 3,
-      name: "contact3",
-      email: "contact3@gmail.com",
-      phone: "3333",
-      job: "",
-    },
-  ]);
+  const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem("contacts") || "[]"));
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts])
   return (
     <>
       {currentPage === "contact-list" && (
