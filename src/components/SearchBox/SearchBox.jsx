d60e6082504a40fde777dadd36de1c499307b091
Reyhaneh Nasiri <reyhaneh.6627@gmail.com>
2025-10-25 03:33:06 +0330

    feat(search): add search functionality

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,9 +1,15 @@
-import styles from "./SearchBox.module.css"
-const SearchBox = () => {
+import styles from "./SearchBox.module.css";
+const SearchBox = ({ setSearch }) => {
   return (
     <div className={styles.searchBox}>
-      <input type="text" placeholder="Search contacts..." />
-      <button><i className="fa-solid fa-search"></i></button>
+      <input
+        type="text"
+        placeholder="Search contacts..."
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button>
+        <i className="fa-solid fa-search"></i>
+      </button>
     </div>
   );
 };
